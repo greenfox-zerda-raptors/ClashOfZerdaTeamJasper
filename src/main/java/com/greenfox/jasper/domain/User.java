a8c882b0fc7081510201d0b3cf23d9a036f8b093
bncbodrogi <bncbodrogi@gmail.com>
2017-01-23 09:22:19 +0100

    pstgresql dependencies added

---
+++ src/main/java/com/greenfox/jasper/domain/User.java
@@ -0,0 +1,8 @@
+package com.greenfox.jasper.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+}
---
+++ src/main/java/com/greenfox/jasper/repos/UserRepo.java
@@ -0,0 +1,6 @@
+package com.greenfox.jasper.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepo extends CrudRepository<User, Long> {
+}
