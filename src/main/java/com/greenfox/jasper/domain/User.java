434bc6c81d3827d3b9a8d1138680c6cbd39cdd21
bncbodrogi <bncbodrogi@gmail.com>
2017-01-23 10:21:53 +0100

    user class and user repo interface added

--- src/main/java/com/greenfox/jasper/domain/User.java
+++ src/main/java/com/greenfox/jasper/domain/User.java
@@ -1,8 +1,19 @@
 package com.greenfox.jasper.domain;
 
+import org.springframework.stereotype.Component;
+
 import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
+@Component
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String password;
+
 }
+
