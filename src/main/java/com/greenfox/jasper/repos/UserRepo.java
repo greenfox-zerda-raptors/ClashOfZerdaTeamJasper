8da0a72733477ab794aa5feec04d13390d21425c
bncbodrogi <bncbodrogi@gmail.com>
2017-01-23 10:53:41 +0100

    postgresql configured to run locally

--- src/main/java/com/greenfox/jasper/repos/UserRepo.java
+++ src/main/java/com/greenfox/jasper/repos/UserRepo.java
@@ -1,5 +1,6 @@
 package com.greenfox.jasper.repos;
 
+import com.greenfox.jasper.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends CrudRepository<User, Long> {
