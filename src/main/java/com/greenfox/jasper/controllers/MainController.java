6defed85a8fb558540a963303db390e63e43bccd
bncbodrogi <bncbodrogi@gmail.com>
2017-01-25 13:59:20 +0100

    heroku test

--- src/main/java/com/greenfox/jasper/controllers/MainController.java
+++ src/main/java/com/greenfox/jasper/controllers/MainController.java
@@ -1,7 +1,9 @@
 package com.greenfox.jasper.controllers;
 
-import com.google.gson.Gson;
+import com.greenfox.jasper.domain.Building;
+import com.greenfox.jasper.domain.GameEvent;
 import com.greenfox.jasper.domain.User;
+import com.greenfox.jasper.services.EventServices;
 import com.greenfox.jasper.services.MainServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,12 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 @RequestMapping (value = "/", method = RequestMethod.GET)
 public class MainController {
     
   @Autowired
     MainServices mainServices;
+
+  @Autowired
+    EventServices eventServices;
 
     @RequestMapping(value = "/kingdom/{kingdomName}", method = RequestMethod.GET)
     public User getKingdom(@PathVariable String kingdomName){
@@ -24,5 +32,16 @@ public class MainController {
     @RequestMapping(value = "/user/{userId}", method = RequestMethod.GET)
     public User getUser(@PathVariable int userId){
         return mainServices.findOneUser(userId);
+    }
+
+    @RequestMapping(value = "/building/levelup/{buildingId}", method = RequestMethod.POST)
+    public void levelUpBuildingById(@PathVariable int buildingId, HttpServletResponse response) throws IOException {
+        eventServices.executeEvent(buildingId, GameEvent.LEVELUP);
+        response.sendRedirect(String.format("/building/%d", buildingId));
+    }
+
+    @RequestMapping(value = "/building/{buildingId}", method = RequestMethod.GET)
+    public Building getBuilding(@PathVariable int buildingId) {
+        return mainServices.findOneBuilding(buildingId);
     }
 }
