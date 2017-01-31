4905465b6ea35d8f14306656f67a3d0e6d134df4
bncbodrogi <bncbodrogi@gmail.com>
2017-01-31 09:37:27 +0100

    basic endpoints done, small issues corrected going to merge

--- src/main/java/com/greenfox/jasper/controllers/UserController.java
+++ src/main/java/com/greenfox/jasper/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.greenfox.jasper.controllers;
 
 import com.greenfox.jasper.domain.User;
 import com.greenfox.jasper.services.MainServices;
-import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
