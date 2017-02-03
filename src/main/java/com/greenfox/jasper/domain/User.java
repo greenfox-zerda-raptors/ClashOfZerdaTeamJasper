8cd219487987f7197367760ef684bcd191a51eee
WarWolf89 <almasi.csaba89@gmail.com>
2017-02-03 12:53:28 +0100

    heroku restart

--- src/main/java/com/greenfox/jasper/domain/User.java
+++ src/main/java/com/greenfox/jasper/domain/User.java
@@ -20,9 +20,7 @@ public class User implements Serializable {
 
     @Column(name = "user_name")
     private String name;
-
-
-
+    
     // TODO do we need this?
     @JsonBackReference
     @OneToOne (mappedBy = "user", cascade = CascadeType.ALL)
