64db4594a6fb6f19d399dd9ddf627d3d4f5fcb78
Z-Hamza <hamzazoltan@citromail.hu>
2017-02-08 13:02:35 +0100

    added todo

--- src/main/java/com/greenfox/jasper/services/DTOServices.java
+++ src/main/java/com/greenfox/jasper/services/DTOServices.java
@@ -16,6 +16,8 @@ public class DTOServices {
 
     ModelMapper modelMapper = new ModelMapper();
 
+    // TODO Battle
+
     public com.greenfox.jasper.dto.KingdomDto convertKingdomToDTO(Kingdom kingdom) {
         KingdomDto kingdomDto = new KingdomDto();
         kingdomDto.setUser(convertUserToDto(kingdom.getUser()));
