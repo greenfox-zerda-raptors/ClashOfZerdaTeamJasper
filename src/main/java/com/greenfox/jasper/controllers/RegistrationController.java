a79dee3223c5ed87ece3bf6a3dd1e5a765376092
bncbodrogi <bncbodrogi@gmail.com>
2017-02-08 09:55:15 +0100

    registration controller import error fixed

--- src/main/java/com/greenfox/jasper/controllers/RegistrationController.java
+++ src/main/java/com/greenfox/jasper/controllers/RegistrationController.java
@@ -7,7 +7,6 @@ import com.greenfox.jasper.services.KingdomServices;
 import com.greenfox.jasper.services.RegistrationServices;
 import com.greenfox.jasper.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import sun.plugin2.message.Message;
 
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = "/", method = RequestMethod.GET)
