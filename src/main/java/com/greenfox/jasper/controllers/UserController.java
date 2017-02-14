a818427625e843e361fd21f237ad37086f20c2fa
WarWolf89 <almasi.csaba89@gmail.com>
2017-02-14 10:50:17 +0100

    multiple user controllers fixed

--- src/main/java/com/greenfox/jasper/controllers/UserController.java
+++ src/main/java/com/greenfox/jasper/controllers/UserController.java
@@ -1,31 +1,27 @@
 package com.greenfox.jasper.controllers;
 
-import com.greenfox.jasper.domain.User;
+
 import com.greenfox.jasper.dto.KingdomDto;
-import com.greenfox.jasper.security.JwtUser;
 import com.greenfox.jasper.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping(value = "/", method = RequestMethod.GET)
 public class UserController {
 
     @Autowired
     private UserServices userServices;
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public User getUser(@AuthenticationPrincipal JwtUser currentUser) {
-        long userId = currentUser.getId();
-        return userServices.findOneUser(userId);
-    }
+//    @RequestMapping(value = "/user", method = RequestMethod.GET)
+//    public User getUser(@AuthenticationPrincipal JwtUser currentUser) {
+//        long userId = currentUser.getId();
+//        return userServices.findOneUser(userId);
+//    }
 
     @RequestMapping(value = "/search/username", method = RequestMethod.GET)
     public ResponseEntity<KingdomDto> userSearch(@RequestParam("username") String username) {
