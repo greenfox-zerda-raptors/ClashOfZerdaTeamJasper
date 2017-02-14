99497994a8168d7713263d862ad5762fa8875213
WarWolf89 <almasi.csaba89@gmail.com>
2017-02-14 10:23:08 +0100

    custom CORS filter added

--- src/main/java/com/greenfox/jasper/controllers/UserController.java
+++ src/main/java/com/greenfox/jasper/controllers/UserController.java
@@ -1,8 +1,6 @@
 package com.greenfox.jasper.controllers;
 
-import com.greenfox.jasper.domain.Kingdom;
 import com.greenfox.jasper.domain.User;
-import com.greenfox.jasper.dto.BuildingDto;
 import com.greenfox.jasper.dto.KingdomDto;
 import com.greenfox.jasper.security.JwtUser;
 import com.greenfox.jasper.services.UserServices;
@@ -10,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -27,7 +28,7 @@ public class UserController {
     }
 
     @RequestMapping(value = "/search/username", method = RequestMethod.GET)
-    public ResponseEntity<KingdomDto> userSearch(@RequestParam ("username")String username){
+    public ResponseEntity<KingdomDto> userSearch(@RequestParam("username") String username) {
         KingdomDto result = userServices.returnKingdomByUsername(username);
         if (result == null) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
