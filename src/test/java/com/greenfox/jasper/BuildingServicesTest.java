6ac4446211b1358e62796356a15906c21ce00483
Z-Hamza <hamzazoltan@citromail.hu>
2017-02-15 20:23:15 +0100

    refactored, further tests

--- src/test/java/com/greenfox/jasper/BuildingServicesTest.java
+++ src/test/java/com/greenfox/jasper/BuildingServicesTest.java
@@ -1,8 +1,11 @@
 package com.greenfox.jasper;
 
 import com.greenfox.jasper.domain.Building;
+import com.greenfox.jasper.domain.Kingdom;
 import com.greenfox.jasper.repos.BuildingRepo;
+import com.greenfox.jasper.repos.KingdomRepo;
 import com.greenfox.jasper.services.BuildingServices;
+import com.greenfox.jasper.services.KingdomServices;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,6 +17,8 @@ import java.util.List;
 import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -26,9 +31,19 @@ public class BuildingServicesTest{
     @InjectMocks
     private BuildingServices buildingServices;
 
+    @InjectMocks
+    private KingdomServices kingdomServices;
+
+    @Mock
+    private KingdomRepo kingdomRepo;
+
     @Mock
     private BuildingRepo buildingRepo;
 
+    private final List<Building> testBuildingList = new ArrayList<>(
+            Arrays.asList(
+                    new Building("townhall"), new Building("mine"), new Building("farm"), new Building("barrack")
+            ));
 
     @Test
     public void findOneBuildingTest(){
@@ -46,19 +61,37 @@ public class BuildingServicesTest{
     }
 
     @Test
-    public void findAll(){
+    public void findAllBuildingsTest(){
         initMocks(this);
-        List<Building> testList = new ArrayList<>(
-                Arrays.asList(
-                new Building("townhall"), new Building("mine"), new Building("farm"), new Building("barrack")
-        ));
-        when(buildingRepo.findAll()).thenReturn(testList);
+        when(buildingRepo.findAll()).thenReturn(testBuildingList);
         List<Building> obtainedList = buildingServices.findAllBuildings();
         assertNotNull(obtainedList);
         assertEquals("mine", obtainedList.get(1).getType());
         assertEquals(4, obtainedList.size());
         assertEquals("farm", obtainedList.get(2).getType());
         assertEquals(0, obtainedList.get(3).getLevel());
+
+    }
+
+    @Test
+    public void findAllBuildingsByKingdomByTypeTest(){
+        initMocks(this);
+        Kingdom testKingdom = new Kingdom();
+        testKingdom.setKingdomId(20);
+        List<Building> testFarmList = new ArrayList<>(Arrays.asList(
+           new Building("farm"), new Building("farm")
+        ));
+       
+        when(kingdomRepo.findOne(20L)).thenReturn(testKingdom);
+        when(buildingRepo.findAllBuildingByKingdomAndType(any(Kingdom.class), eq("farm") )).thenReturn(testFarmList);
+        List<Building> obtainedFarms = buildingServices.findAllBuildingByKingdomIdAndByType(20L, eq("farm"));
+        assertNotNull(obtainedFarms);
+        assertEquals(2, obtainedFarms.size());
+        assertEquals("farm", obtainedFarms.get(0).getType());
+
+//        List<Building> obtainedGeneric = buildingServices.findAllBuildingByKingdomIdAndByType(20L, "barrack");
+//        assertNull(obtainedGeneric);
+
 
     }
 }
