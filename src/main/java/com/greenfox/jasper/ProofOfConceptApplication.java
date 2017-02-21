976561cd6fbf9d54bd76dd0fec07e285fe445911
WarWolf89 <almasi.csaba89@gmail.com>
2017-02-21 15:41:12 +0100

    cleanup2

--- src/main/java/com/greenfox/jasper/ProofOfConceptApplication.java
+++ src/main/java/com/greenfox/jasper/ProofOfConceptApplication.java
@@ -2,14 +2,13 @@ package com.greenfox.jasper;
 
 import com.greenfox.jasper.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ProofOfConceptApplication{
+public class ProofOfConceptApplication {
 
     @Autowired
     KingdomServices kingdomServices;
