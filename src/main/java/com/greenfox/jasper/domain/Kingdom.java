ce5540ea580241263e50884613133ed910ab2a6b
Adam <veroci@gmail.com>
2017-02-22 17:09:28 +0100

    refactored

--- src/main/java/com/greenfox/jasper/domain/Kingdom.java
+++ src/main/java/com/greenfox/jasper/domain/Kingdom.java
@@ -24,6 +24,7 @@ public class Kingdom implements Serializable {
 
     @JsonManagedReference
     @OneToOne
+    @JoinColumn(name="user_id")
     private User user;
 
     @OneToMany(mappedBy = "kingdom", cascade = CascadeType.ALL)
