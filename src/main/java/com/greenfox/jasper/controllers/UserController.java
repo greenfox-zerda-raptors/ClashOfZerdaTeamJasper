7ea8adc6f7d2420050482b7a818394aa95eeae20
Adam <veroci@gmail.com>
2017-02-22 14:34:09 +0100

    refactor

--- src/main/java/com/greenfox/jasper/controllers/UserController.java
+++ src/main/java/com/greenfox/jasper/controllers/UserController.java
@@ -6,10 +6,7 @@ import com.greenfox.jasper.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -17,17 +14,8 @@ public class UserController {
     @Autowired
     private UserServices userServices;
 
-//    @RequestMapping(value = "/user", method = RequestMethod.GET)
-//    public User getUser(@AuthenticationPrincipal JwtUser currentUser) {
-//        long userId = currentUser.getHttpStatusCode();
-//        return userServices.findOneUser(userId);
-//    }
-
-
-
-    // TODO Frontend request / do we need to give back everything?
-    @RequestMapping(value = "/search/username", method = RequestMethod.GET)
-    public ResponseEntity<KingdomDto> userSearch(@RequestParam("username") String username) {
+    @RequestMapping(value = "/search/{username}", method = RequestMethod.GET)
+    public ResponseEntity<KingdomDto> userSearch(@PathVariable("username") String username) {
         KingdomDto result = userServices.returnKingdomByUsername(username);
         if (result == null) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
